df785fcc2f2e6d1adbbc9daf0dd04a03195b0a12
ncatlandarch-droid <ncatlandarch@gmail.com>
2026-07-24 19:06:54 -0400

    Update Plaid functions with matched Client ID and Sandbox secret

--- netlify/functions/exchange-public-token.js
+++ netlify/functions/exchange-public-token.js
@@ -1,11 +1,11 @@
 const { Configuration, PlaidApi, PlaidEnvironments } = require('plaid');
 
-const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID || '0df407f5dafe42da809d45ece1f468';
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID || '6a63edaf00a3b9000d8114e4';
 const PLAID_SECRET = process.env.PLAID_SECRET || 'f55a7107f90ae5b48c3e853f6c7eb4';
 const PLAID_ENV = process.env.PLAID_ENV || 'sandbox';
 
 const configuration = new Configuration({
-  basePath: PlaidEnvironments[PLAID_ENV],
+  basePath: PlaidEnvironments[PLAID_ENV] || PlaidEnvironments.sandbox,
   baseOptions: {
     headers: {
       'PLAID-CLIENT-ID': PLAID_CLIENT_ID,
@@ -31,7 +31,6 @@ exports.handler = async function(event, context) {
     const accessToken = tokenResponse.data.access_token;
     const itemId = tokenResponse.data.item_id;
 
-    // Fetch accounts & real balance data
     const accountsResponse = await client.accountsGet({
       access_token: accessToken,
     });
